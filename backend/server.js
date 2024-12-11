963b2951c5d419b0fab269890648c6508eec06c0
vincentvicente <145320968+vincentvicente@users.noreply.github.com>
2024-12-10 17:57:09 -0800

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const path = require('path');
 const userRoutes = require('./routes/user.routes');
 const statusRoutes = require('./routes/status.routes');
 const cors = require('cors');
 require('./db');
 
 const app = express();
+
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173',
+      'https://qiyuan-zhu-liangyou-xu-project3.onrender.com',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
@@ -15,11 +20,17 @@ app.use(
 );
 
 app.options('*', cors());
-
 app.use(express.json());
+
+const frontendPath = path.join(__dirname, '../frontend/dist');
+app.use(express.static(frontendPath));
 
 app.use('/api/users', userRoutes);
 app.use('/api/statuses', statusRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(frontendPath, 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
