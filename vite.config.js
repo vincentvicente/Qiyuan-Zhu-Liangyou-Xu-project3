a47124041e952627a8885b24b171a60e479302a3
vincentvicente <145320968+vincentvicente@users.noreply.github.com>
2024-12-10 16:51:27 -0800

    update vite and index.html

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  root: "./frontend",
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000/',
-        changeOrigin: true,
-      }
-    }
-  }
-})
+export default defineConfig(({ mode }) => {
+  const isDev = mode === 'development';
+
+  return {
+    root: "./frontend",
+    plugins: [react()],
+    ...(isDev && {
+      server: {
+        proxy: {
+          '/api': {
+            target: 'http://localhost:3000/',
+            changeOrigin: true,
+          },
+        },
+      },
+    }),
+  };
+});
